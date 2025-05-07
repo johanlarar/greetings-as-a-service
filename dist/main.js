cf53f2df673c51f4ed335cd0572c901495753b7a
johan_larare <johan.saldes@lararvikare.nu>
2025-05-07 08:42:33 +0200

    Added list on loading

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 console.log("Hello, from main.js");
 
 let firstName = "";
+const listOfNames = [];
 
 const getMessage = async () => {
 	const res = await fetch(`/hello/${firstName}`);
@@ -9,8 +10,18 @@ const getMessage = async () => {
 	document.getElementById("heading").innerHTML = greeting;
 };
 
+const getMessages = async () => {
+	const res = await fetch("/data");
+	const names = await res.json();
+	for (n of names) {
+		listOfNames.push(n);
+	}
+};
+
 const input = document.getElementById("your_name");
 const button = document.getElementById("submit");
+const list = document.getElementById("name-list");
+
 button.addEventListener("click", () => {
 	getMessage();
 });
@@ -18,4 +29,20 @@ button.addEventListener("click", () => {
 input.addEventListener("change", (event) => {
 	const inputValue = event.target.value;
 	firstName = inputValue;
+});
+
+document.addEventListener("DOMContentLoaded", async (event) => {
+	await getMessages();
+	if (!listOfNames.length) {
+		const el = document.createElement("li");
+		el.innerHTML = "No available data";
+		console.log({ list: event.target });
+		list.appendChild(el);
+	} else {
+		for (n of listOfNames) {
+			const el = document.createElement("li");
+			el.innerHTML = n.name;
+			list.appendChild(el);
+		}
+	}
 });
