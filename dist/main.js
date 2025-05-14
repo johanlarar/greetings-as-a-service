a3c41b2d701630f7f01e3a181b82408aff5e2de6
johan_larare <johan.saldes@lararvikare.nu>
2025-05-14 15:24:58 +0200

    Added comments to the app

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,18 @@
 console.log("Hello, from main.js");
 
-let owner = "";
-let todo = "";
+// Our global variables
+
+// The complete list of todos available to us
 let todos = [];
 
+// Keep track of what's been written in our inputs
+let owner = "";
+let todo = "";
+
+// Select the HTML element that we start of with by default
+// that is our inputs, submit button, the list in which we want
+// to render our todos, the loading message and a placeholder for
+// any potential errors we want to inform the end user of.
 const ownerInput = document.getElementById("owner");
 const todoInput = document.getElementById("task");
 const button = document.getElementById("submit");
@@ -11,7 +20,14 @@ const list = document.getElementById("todos");
 const loadingMessage = document.getElementById("loading");
 const errorMessage = document.getElementById("error");
 
+/**
+ * @name createTodo
+ * @description Creates a todo item in our backend
+ */
 const createTodo = async () => {
+	// Fetch returns a Promise. Sometime in the future this will resolve. Javascript can do
+	// other things (like rendering the UI) in the meanwhile and come back later to check if
+	// the Promise resolved.
 	const res = await fetch("/todo", {
 		method: "POST",
 		body: JSON.stringify({
@@ -22,19 +38,23 @@ const createTodo = async () => {
 			"Content-Type": "application/json",
 		},
 	});
+	// Again, await to resolve the JSON from the body of the response
 	const body = await res.json();
+	// If we recieve anything but a 20x (200 etc.) of some sort throw an error
 	if (!res.ok) {
 		throw new Error(body.error);
 	}
 	const greeting = body.message; // "Hello, test"
 	document.getElementById("heading").innerHTML = greeting;
-	renderTodos();
+
 	// Empty inputs
 	ownerInput.value = "";
 	todoInput.value = "";
 	// Empty values in memory
 	owner = "";
 	todo = "";
+	// Rerender the todo list
+	renderTodos();
 };
 
 /**
@@ -42,12 +62,20 @@ const createTodo = async () => {
  * @description Gets a list of todos
  */
 const getTodos = async () => {
+	// When fetching, if we don't provide an option object as the second parameter
+	// it default to method as GET.
+	// fetch("/todos", { method: 'GET' }) is the same as what we've written bellow.
+	// When we want to GET we don't provide an option object.
 	const res = await fetch("/todos");
 	const body = await res.json();
+
+	// If we recieve a status code that's anything but a 20x (200 etc.) throw an error
 	if (!res.ok) {
 		throw new Error(body.error);
 	}
+	// We overwrite the todos array with our new value
 	todos = body;
+	// We DO NOT rerender the list here because we use this function inside of the renderTodos function
 };
 
 /**
@@ -55,10 +83,14 @@ const getTodos = async () => {
  * @description Updates checked for todo by id
  */
 const updateChecked = async (event) => {
+	// Here we wanted to do a PUT request so we added a second option object with the
+	// property method set to PUT. Since this endpoint is just a toggle true/false we
+	// don't need to provide any additional data.
 	const res = await fetch(`/todo/checked/${event.target.id}`, {
 		method: "PUT",
 	});
 	const body = res.json();
+	// If we recieve a status code that's anything but a 20x (200 etc.) throw an error
 	if (!res.ok) {
 		throw new Error(body.error);
 	}
@@ -71,6 +103,10 @@ const updateChecked = async (event) => {
  */
 
 const updatePrio = async (event) => {
+	// This time we wanted to update a priority that ha 3 states, 1st, 2nd and 3rd.
+	// That means that we need to provide a body to our options object. The body is
+	// just a text string but we want it to be a text string representing the JSON
+	// of our object containing a property prio.
 	const res = await fetch(`/todo/prio/${event.target.id}`, {
 		method: "PUT",
 		body: JSON.stringify({
@@ -81,9 +117,11 @@ const updatePrio = async (event) => {
 		},
 	});
 	const body = res.json();
+	// If we recieve a status code that's anything but a 20x (200 etc.) throw an error
 	if (!res.ok) {
 		throw new Error(body.error);
 	}
+	// We rerender the todo list to reflect the updated state
 	renderTodos();
 };
 
@@ -92,18 +130,37 @@ const updatePrio = async (event) => {
  * @description Deletes todo by id
  */
 const deleteTodo = async (event) => {
+	// To send a delete request we provide a options object with the property method set to DELETE.
 	await fetch(`/todo/delete/${event.target.id}`, {
 		method: "DELETE",
 	});
+	// We rerender the list to reflect the new state
 	renderTodos();
 };
 
 const createLiNoDataAvailable = () => {
 	const container = document.createElement("li");
-	container.textContent = "No data available";
+	container.textContent = "No todos available. Please create one!";
 	return container;
 };
 
+// By creating isolated functions for each section of our list item
+// we're able to compartmentalize each section in to it's own scope.
+// This means that we can reson more clearly about the code of each
+// section. Now easy is relative but easier than if we had all this
+// logic running in one huge function.
+
+// The input element needs a few things. For one it needs a container element,
+// i.e. the div element that wrapps it. Then we need to set the type of the input
+// so that our browser knows to render a checkbox. We want to set the id so that
+// we can use that id when executing the updateChecked function to know which
+// todo we should toggle the done state for.
+//
+// Finally we want to add an event listener and return the container now containing
+// our input[type=checkbox] element with its corresponding event listener.
+//
+// This is essentially what we do for all of our elements in the todo list. Now if we
+// want to add another element we have a code structure to follow in how to do so.
 const createCheckBoxColumn = (item) => {
 	const container = document.createElement("div");
 	const checkbox = document.createElement("input");
@@ -118,9 +175,11 @@ const createCheckBoxColumn = (item) => {
 	return container;
 };
 
+// Now we want to add a text node with owner and the todo. If the checkbox is set to true
+// and the todo is done we want to give the item a strike through marking it as done.
 const createTextNodeColumn = (item) => {
 	const container = document.createElement("div");
-	container.textContent = `${item.owner} ${item.todo}`;
+	container.textContent = `${item.owner} - ${item.todo}`;
 	if (item.checked) {
 		container.style.textDecoration = "line-through";
 	} else {
@@ -129,6 +188,8 @@ const createTextNodeColumn = (item) => {
 	return container;
 };
 
+// Here we add the delete button and if you've followed in the comments this far
+// it should be fairly straight forward in understanding what this does.
 const createDeleteButtonColumn = (id) => {
 	const container = document.createElement("div");
 	const button = document.createElement("button");
@@ -142,20 +203,41 @@ const createDeleteButtonColumn = (id) => {
 	return container;
 };
 
+// Now here it gets a bit trickier. We want to render a drop down using the browser
+// native select element.
 const createPrioDropdown = (item) => {
 	const container = document.createElement("div");
 	const dropdown = document.createElement("select");
 	dropdown.id = item.id;
+	// Most things up to this point have been familiar but now there's an array.
+	// We can assume it represents our various priority levels 1, 2 and 3.
 	const prioArr = [1, 2, 3];
 	for (prio of prioArr) {
+		// when creating a select element we get a HTMLSelectElement in return. This element
+		// has support for a Collection of Option. To add a <option> element we can go:
+		// const option = document.createElement("option");
+		// option.value = "some value";
+		// option.textContent = "Some text";
+		// dropdown.appendChild(option);
+		//
+		// or... you guessed it. We can use the Option class and add to our dropdown
+		// using the options collection that exports a method add. They do the same thing.
+		// See: https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptionElement/Option
 		dropdown.options.add(new Option(prio, prio));
 	}
-	dropdown.value = item.prio;
-	dropdown.addEventListener("change", updatePrio);
+	// To set which value to display we have to set the value here, corresponding to the
+	// value which we set in the Option. If we do it prior to creating the options list our
+	// select element doesn't know which value corresponds to which option element (because there are none)
+	dropdown.value = item.prio; // Sets the default value of the select dropdown to our value from the backend
+	dropdown.addEventListener("change", updatePrio); // Add an event listener
 	container.appendChild(dropdown);
 	return container;
 };
 
+// Check the HTML content in your browser
+// We created a li element, inside of it we have four div elements.
+// Each of these div elements have a checkbox, a priority drop down, the name and task
+// followed by the button, in that order.
 const createLiElementRow = (item) => {
 	const container = document.createElement("li");
 	container.appendChild(createCheckBoxColumn(item));
@@ -165,6 +247,17 @@ const createLiElementRow = (item) => {
 	return container;
 };
 
+// We've not talked too much about sort but I want you to understand just one
+// thing here. It's that there's several different ways of sorting things in programming
+// which will all give you the same result – a sorted list of some type.
+// To see a visualisation of various sorting algorithm check this video out:
+// https://www.youtube.com/watch?v=kPRA0W1kECg
+// What you need to understand is that some sorting algorithm are better suited for some
+// type of data. Also different browsers may use different algorithms.
+// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+//
+// We're not implementing a custom algorithm here we're just telling the sort function what to sort
+// on in our object. We want to sort based on priority so we tell it to compare and sort using prio.
 const sortTodosByPrio = () =>
 	todos.sort((a, b) => {
 		if (a.prio < b.prio) {
@@ -177,22 +270,32 @@ const sortTodosByPrio = () =>
 		return 0;
 	});
 
-const renderTodos = async (event) => {
+// This function is very central to our rendering of the application
+const renderTodos = async () => {
 	try {
+		// Remember that in getTodos we don't run renderTodos since it's being
+		// called inside renderTodos already.
 		await getTodos();
+		// We need to empty the list otherwise we will just create duplicates
 		list.innerHTML = "";
-		if (!todos.length) {
-			const el = createLiNoDataAvailable();
-			list.appendChild(el);
-		} else {
-			loadingMessage.remove();
+
+		// If there are no todos in our array it has a length of 0 evaluating to false
+		// if it does have items we will render them.
+		if (todos.length) {
+			loadingMessage.remove(); // By default there's a loading message. Remove that.
+			// loop through the sorted list of todos for each iteration assing current item to
+			// the variable nameItem.
 			for (nameItem of sortTodosByPrio()) {
-				const listElement = createLiElementRow(nameItem);
-				list.appendChild(listElement);
+				const listElement = createLiElementRow(nameItem); // Call our function to create a list element
+				list.appendChild(listElement); // Append the list element to our ul list
 			}
+		} else {
+			// If not data was available i.e. if the todos array is empty
+			const el = createLiNoDataAvailable(); // Render the "not available"" message
+			list.appendChild(el); // Append it to our ul list
 		}
 	} catch (error) {
-		errorMessage.textContent = error.message;
+		errorMessage.textContent = error.message; // If there's an error of any type write it to our error field
 	}
 };
 
@@ -236,4 +339,12 @@ ownerInput.addEventListener("keypress", (event) => handleEnter(event, "owner"));
 
 todoInput.addEventListener("keypress", (event) => handleEnter(event, "todo"));
 
+// While last thing in the code this is actually amongst the first that exectues.
+// Once the DOM tree has loaded and the users has recieved the entire DOM structure
+// javascript fires the event "DOMContentLoaded". By listening to it we can make sure
+// that we only start executing our javascript once the entire DOM tree is rendered.
+// If we don't do this we risk that part of the DOM isn't available and some of our
+// selectors might return null.
+// For example if our ul list isn't rendered upon execution we can't select it and
+// assign it to `list` meaning we aren't able to render any todos.
 document.addEventListener("DOMContentLoaded", renderTodos);
--- server.js
+++ server.js
@@ -9,14 +9,23 @@ const app = express();
 
 const fileName = path.join(__dirname, "data", "name.json");
 
+/**
+ * @description This function opens a file in the file system containing JSON and loads the JSON into memory
+ * @return {object} The JSON structure defined in data/name.json
+ * @notice data/name.json needs to be manually initialized with at least "[]" i.e. an empty array
+ */
 function readJsonFile() {
 	const file = fs.readFileSync(fileName, {
-		encoding: "utf-8",
-		flag: "r+",
+		encoding: "utf-8", // Encoding is turning information into a format that a computer can understand.
+		flag: "r+", // Notice this flag https://nodejs.org/api/fs.html#file-system-flags
 	});
 	return JSON.parse(file);
 }
 
+/**
+ * @description Writes an object as JSON to a file in the file system
+ * @param {object} The object that is to be written
+ */
 function wrtieToJsonFile(data) {
 	fs.writeFileSync(fileName, JSON.stringify(data));
 }
@@ -24,6 +33,9 @@ function wrtieToJsonFile(data) {
 app.use(express.static(path.join(__dirname, "dist")));
 app.use(bodyParser.json());
 
+/**
+ * @description A custom error object, extends Error
+ */
 class ValidationError extends Error {
 	constructor(message) {
 		super(message);
@@ -31,6 +43,9 @@ class ValidationError extends Error {
 	}
 }
 
+/**
+ * @description This enpoint creates (C in CRUD) a todo.
+ */
 app.post("/todo", (req, res) => {
 	try {
 		if (!req.body.owner) {
@@ -53,43 +68,65 @@ app.post("/todo", (req, res) => {
 
 		res.send({ message: `Hello, ${req.body.owner}` });
 	} catch (error) {
+		// `error` is a class. Defaults to the built in javascript class Error
+		// We can extend Error to create our own custom error handling
+
+		// We always log the error to the backend. We log it as JSON so it can be machine parse later on.
 		console.error(JSON.stringify({ error: error.message, fn: "/todo" }));
+		// At the next line we're checking if `error` is an instance of ValidationError
+		// which is our custom error defined above.
 		if (error instanceof ValidationError) {
+			// If `error` is an instance of ValidationError then we know more of what type of error it is
+			// we know that we can safely present the error to the end user without risking to give away
+			// implementation details (a security risk) or confusing the end user with error messages designed
+			// for us as developers.
+
+			// We send the status code 400 (Bad Request) because it was something wrong with their request.
+			// Maybe they forgot to subit at owner or a todo
 			res.status(400).send({ error: error.message });
 		} else {
+			// If it was not a ValidationError it was any other type of error and we provide the end user
+			// with a "friendly" error message (meaning not an error message design for us a as engineers)
+			// We use 500 (Internal Server Error) as the error code. This can be useful for support.
 			res.status(500).send({ error: "Unable to write new names" });
 		}
 	}
 });
 
+/**
+ * @description This endpoint reads (R in CRUD) a list of todos
+ */
 app.get("/todos", (req, res) => {
 	try {
-		const data = readJsonFile();
-		res.send(data);
+		const data = readJsonFile(); // We read and load in our JSON structure from the file system to memory
+		res.send(data); // We send that to the end user. We haven't specified status code so it will be 200 (OK)
 	} catch (error) {
 		console.log(JSON.stringify({ error: error.message, fn: "/names" }));
 		res.status(500).send({ error: "Unable to load names" });
 	}
 });
 
-app.delete("/todo/delete/:id", (req, res) => {
-	try {
-		const data = readJsonFile().filter((d) => d.id !== req.params.id);
-
-		wrtieToJsonFile(data);
-		res.send(data);
-	} catch (error) {}
-});
-
+/**
+ * @description This endpoints updates (U in CRUD) the checked state of a todo by id
+ */
 app.put("/todo/checked/:id", (req, res) => {
 	try {
+		// `map` create a new array from a given array, in our case the data from our json file.
+		// If it does, we update it. We simply toggle it by setting it to the opposite of what it was
+		// set to before. If it was checked (i.e. if it was true) we set it to "not true" which is false
+		// therefor if it was false we set it to "not false" which is true. This create a toggle between
+		// the two states checked or unchecked.
+		// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+
 		const data = readJsonFile().map((data) => {
 			if (data.id === req.params.id) {
 				data.checked = !data.checked;
 			}
 			return data;
 		});
-		wrtieToJsonFile(data);
+		wrtieToJsonFile(data); // We now write our entire data structure to our file system
+		// We use filter to return a new array which consist of only the items in which our return resulted in true
+		// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 		res.json(data.filter((d) => d.id === req.params.id));
 	} catch (error) {
 		console.log(error);
@@ -99,18 +136,30 @@ app.put("/todo/checked/:id", (req, res) => {
 	}
 });
 
+/**
+ * @description This endpoint updates (U in CRUD) a priority of a todo by id
+ */
 app.put("/todo/prio/:id", (req, res) => {
 	try {
 		if (req.body.prio > 3) {
-			throw new ValidationError("Prio can't be larger than 3");
+			throw new ValidationError("Prio can't be larger than 3"); // Throw our custom error
 		}
+		// `map` create a new array from a given array, in our case the data from our json file.
+		// If it does, we update it. We simply toggle it by setting it to the opposite of what it was
+		// set to before. If it was checked (i.e. if it was true) we set it to "not true" which is false
+		// therefor if it was false we set it to "not false" which is true. This create a toggle between
+		// the two states checked or unchecked.
+		// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 		const data = readJsonFile().map((data) => {
 			if (data.id === req.params.id) {
 				data.prio = Number.parseInt(req.body.prio);
 			}
 			return data;
 		});
+		// Write to file system
 		wrtieToJsonFile(data);
+		// We use filter to return a new array which consist of only the items in which our return resulted in true
+		// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 		res.json(data.filter((d) => d.id === req.params.id));
 	} catch (error) {
 		console.error(
@@ -124,6 +173,22 @@ app.put("/todo/prio/:id", (req, res) => {
 				.send({ error: "Unable to update prio. Try again later..." });
 		}
 	}
+});
+
+/**
+ * @description This endpoint deletes (D in CRUD) a todo by id
+ */
+app.delete("/todo/delete/:id", (req, res) => {
+	try {
+		// By using filter we can now return all the object in which the id is not the same as the id
+		// we've provided as param. This returns a new array containing everything BUT the item we want
+		// to delete.
+		// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+		const data = readJsonFile().filter((d) => d.id !== req.params.id);
+
+		wrtieToJsonFile(data);
+		res.send(data);
+	} catch (error) {}
 });
 
 app.listen(3000, () => {
